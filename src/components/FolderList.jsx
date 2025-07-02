47bd5cfbcc63115757f5f4d7ea20dfb4e53f6ede
maxiamikel19 <maxloversist19@gmail.com>
2025-07-02 14:48:39 -0400

    Estilizar Home e FolderList

--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -1,11 +1,31 @@
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { FaFolder } from "react-icons/fa6";
 
-export default function FolderList({ folders, onEdit, onDelete }) {
+export default function FolderList({ folders, onEdit, onDelete, onClick }) {
   return (
-    <div className="flex items-center justify-start gap-2">
+    <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
       {folders.map((folder) => (
-        <div className="flex items-center justify-center border border-gray-300 p-3 rounded">
-          <FaFolder className="text-amber-700 text-4xl" />
+        <div
+          onClick={onClick}
+          key={folder.id}
+          className=" relative flex flex-col items-center justify-between hover:bg-gray-300  border border-gray-300 p-4 rounded-lg shadow-sm hover:shadow-md transition duration-200 bg-white cursor-pointer"
+        >
+          <FaFolder className="text-amber-700 text-5xl mb-2" />
+
+          <div className="flex flex-col items-center text-center w-full">
+            <p className="text-md font-medium  text-gray-800 mb-2 break-words">
+              {folder.name}
+            </p>
+
+            <div className="absolute top-0 rounded-tr-md right-0 h-auto flex flex-col p-1 text-[10px] items-start font-medium text-gray-600">
+              <p>
+                Files:<span>{folder.totalFiles}</span>
+              </p>
+              <p>
+                Folders:<span>{folder.totalSubFolders}</span>
+              </p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,46 +1,72 @@
 import React, { useEffect, useState } from "react";
 import FolderList from "../components/FolderList";
 import { createFolder, getFolders } from "../api/folder";
+import { RiFolderWarningFill } from "react-icons/ri";
 
 const Home = () => {
   const [folders, setFolders] = useState([]);
+  const [error, setError] = useState("");
   const [formData, setFormData] = useState({
     name: "",
     folderParentId: "",
   });
 
-  const load = () => getFolders().then((res) => setFolders(res.data));
+  const resetFormData = () => {
+    setFormData({
+      name: "",
+      folderParentId: "",
+    });
+  };
+
+  const load = async () => {
+    await getFolders().then((res) => setFolders(res.data));
+  };
 
   useEffect(() => {
     load();
   }, []);
 
-  const handleCreate = (e) => {
+  const handleCreate = async (e) => {
     e.preventDefault();
-    createFolder(formData).then(() => {
+    try {
+      await createFolder(formData);
       load();
-    });
+      resetFormData();
+      setError("");
+    } catch (error) {
+      setError(error?.response?.data?.message || "Erro ao criar pasta");
+    }
   };
 
   return (
-    <div className="max-w-xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Management section</h1>
-      <form onSubmit={handleCreate} className="space-y-4">
-        <div className="flex">
+    <div className="max-w-4xl mx-auto p-4">
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">
+        Management Section
+      </h1>
+
+      <form
+        onSubmit={handleCreate}
+        className="space-y-4 bg-white p-6 rounded-lg shadow-md"
+      >
+        {error && <p className="text-red-500 text-sm">{error}</p>}
+        <div className="flex flex-col sm:flex-row gap-4">
           <input
             type="text"
             placeholder="Folder name"
             value={formData.name}
+            required
             onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            className="w-full border border-gray-300 p-2 rounded"
+            className="flex-1 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
 
           <select
             name="folderParentId"
             id="folderParentId"
+            value={formData.folderParentId || ""}
             onChange={(e) =>
               setFormData({ ...formData, folderParentId: e.target.value })
             }
+            className="flex-1 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
           >
             <option value="">Select parent</option>
             {folders.map((parent) => (
@@ -50,16 +76,32 @@ const Home = () => {
             ))}
           </select>
         </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Save
-        </button>
-      </form>
-      <hr className="my-4" />
 
-      <FolderList folders={folders} onEdit={() => {}} onDelete={() => {}} />
+        <div className="text-right">
+          <button
+            type="submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-md transition"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+
+      {folders.length > 0 ? (
+        <>
+          <hr className="my-6" />
+          <FolderList folders={folders} onEdit={() => {}} onDelete={() => {}} />
+        </>
+      ) : (
+        <>
+          <div className="flex w-fit items-center my-6 relative">
+            <RiFolderWarningFill className="text-9xl text-orange-700" />
+            <span className="absolute top-0 right-0 text-3xl font-bold flex p-3 bg-blue-600 rounded-full outline-0">
+              0
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
